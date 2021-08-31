1e0bedc59a2edb64cd602956582ce1de94f6595d
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-08-31 14:45:34 -0700

    copy editing

--- pages/about.js
+++ pages/about.js
@@ -58,21 +58,21 @@ export default function About() {
 							Led by founder Todd Newberger, we are a design
 							studio committed to innovative, sustainable
 							architecture that elevates the human experience
-							while celebrating the natural environment.
-							Our integrated philosophy is founded on the
-							relationship between the structure and the land it
-							occupies, utilizing the unique characteristics
-							offered by the site.
+							while celebrating the natural environment. Our
+							integrated philosophy is founded on the relationship
+							between the structure and the land it occupies,
+							utilizing the unique characteristics offered by the
+							site.
 							<br />
 							<br />
-							Our 20+ years of coastal planning experience has put
-							us in the unique position of knowing not only how to
+							Our 20+ years of coastal planning experience puts us
+							in the unique position of knowing not only how to
 							design for your coastal space, but also how to
 							navigate the unique challenges coastal building can
 							present. Our expertise is perfectly suited to
-							guiding you through the process of negotiating
-							permits, bluff erosion control, coastal garden
-							planning.
+							guiding you through the process of assembling a
+							skilled team of professionals and delivering a
+							successful project within budget and schedule.
 						</p>
 						<div className='flex flex-col md:flex-row'>
 							<div className='relative w-screen md:w-1/3  '>
@@ -81,41 +81,50 @@ export default function About() {
 									layout='fill'
 									objectFit='contain'
 								/>
-							</div><div className=' ml-12 w-screen md:w-2/3'>
-							<h3 className='text-4xl mb-8'>Todd Newberger</h3>
-							<p className=' text-xl text-[#414141] leading-relaxed font-sans font-normal '>
-								Todd is principal architect, landscape designer,
-								project manager and spearfisherman. Receiving
-								his Bachelor’s degree from University of
-								California at Berkeley, he became a licensed
-								architect in 2007 after spending many years in
-								the industry. His visionary perspective has
-								earned him opportunities to design projects with
-								wide ranging functions. From residential to
-								commercial, big sweeping mansions to intimate
-								cottages, from high end tasting rooms to
-								large-scale medical facilities: all of Todd's
-								projects are as beautiful as they are
-								functional. <br /> <br />He escapes it all by
-								retreating to his vegetable garden by the sea,
-								puttering and planning. Todd's love of the
-								outdoors also calls him to the ocean. Here he
-								can be found freediving in the cold Pacific
-								waters, usually to catch his dinner.<br /> <br />{' '}
-								Todd's deep passion for architecture joins with
-								his love of nature to create wonderful, livable
-								places and spaces for every project he tackles.{' '}
-								<br /><br /> Todd is accredited by the California
-								Architects Board, holds a California Landscape
-								Contractors License, is a Certified Energy
-								Analyst and a Leadership in Energy and
-								Environmental Design Accredited Professional.
-								His work has been featured on Home and Garden
-								Television (HGTV) and he has designed large
-								complex medical complexes for organizations
-								including UC Davis, Sutter Health and Kaiser
-								Permanente.
-							</p></div>
+							</div>
+							<div className=' ml-12 w-screen md:w-2/3'>
+								<h3 className='text-4xl mb-8'>
+									Todd Newberger
+								</h3>
+								<p className=' text-xl text-[#414141] leading-relaxed font-sans font-normal '>
+									Todd is principal architect, landscape
+									designer, project manager and
+									spearfisherman. Receiving his Bachelor’s
+									degree from University of California at
+									Berkeley, he became a licensed architect in
+									2007 after spending many years in the
+									industry. His visionary perspective has
+									earned him opportunities to design projects
+									with wide ranging functions. From modern
+									ocean view homes to intimate cottages, from
+									artfully designed tasting rooms to
+									large-scale medical facilities: all of
+									Todd's projects are as beautiful as they are
+									functional. <br /> <br />
+									He escapes it all by retreating to his
+									vegetable garden by the sea, puttering and
+									planning. Todd's love of the outdoors also
+									calls him to the ocean. Here he can be found
+									freediving in the cold Pacific waters,
+									usually to catch his dinner.
+									<br /> <br /> Todd's deep passion for
+									architecture joins with his love of nature
+									to create wonderful, livable places and
+									spaces for every project he tackles. <br />
+									<br /> Todd is accredited by the California
+									Architects Board and holds a California
+									Landscape Contractors License. His previous
+									experience as a Certified Energy Analyst and
+									a Leadership in Energy and Environmental
+									Design Accredited Professional serve as a
+									foundation for his sustainable ethic. His
+									work has been featured on Home and Garden
+									Television (HGTV) and he has designed large
+									complex medical complexes for organizations
+									including UC Davis, Sutter Health and Kaiser
+									Permanente.
+								</p>
+							</div>
 						</div>
 					</div>
 					<div></div>

912a07fc7eb88a2d185eb9ead42bff8a612942ac
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-11-11 10:23:40 -0800

    testing contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,144 +1,172 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import {
+	useNetlifyForm,
+	NetlifyFormProvider,
+	NetlifyFormComponent,
+	Honeypot,
+} from 'react-netlify-forms'
 import { useForm } from 'react-hook-form'
 
 export default function Contact() {
-	const { register, handleSubmit, formState: { errors } } = useForm()
-	const onSubmit = (data) => {
-		console.log(data)
-	}
+	const { register, handleSubmit, reset, errors } = useForm({
+		mode: 'onBlur',
+	})
+	const netlify = useNetlifyForm({
+		name: 'react-hook-form',
+		action: '/thanks',
+		honeypotName: 'bot-field',
+		onSuccess: (response, context) => {
+			console.log('Successfully sent form data to Netlify Server')
+		},
+	})
+	const onSubmit = (data) => netlify.handleSubmit(null, data)
+
+	const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i
+
 	return (
-		<>
+		<NetlifyFormProvider {...netlify}>
 			<div className='min-h-screen'>
 				<Head>
 					<title>Newberger & Associates | Contact Us</title>
-					<meta property="og:title" content='Newberger & Associates | Contact Us' key="title" />
+					<meta
+						property='og:title'
+						content='Newberger & Associates | Contact Us'
+						key='title'
+					/>
 					<link rel='icon' href='/favicon.ico' />
-					<meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+					<meta charSet='utf-8' />
+					<meta
+						name='viewport'
+						content='initial-scale=1.0, width=device-width'
+					/>
 				</Head>
 
-				<main lang="en">
-				<motion.div
-					initial={{ opacity: 0.2 }}
-					animate={{ opacity: 1 }}
-					className='relative h-screen z-0 bg-[#322121]'>
-					<Image
-						src='v1620327405/Newberger%20and%20Associates/brotherton-06_ivbdxi.jpg'
-						layout='fill'
-						objectFit='cover'
-						className='opacity-50 z-0'
-					/><div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-					<form
-						onSubmit={handleSubmit(onSubmit)}
-						method='post'
-						name='contact'
-						data-netlify='true'
-						netlify-honeypot='bot-field'
-						id='contact'
-						className='md:p-16 p-8 flex flex-col bg-white opacity-100 z-20 drop-shadow-xl w-screen xl:w-[30vw] lg:w-[50vw] md:w-[70vw]'>
-						
-						<h2 className='md:text-2xl text-xl font-sans font-semibold text-center mb-2'>
-							Interested in working together?</h2><h2 className='md:text-2xl text-xl font-sans font-semibold text-center mb-8 md:mb-12'> Let's talk. 707.961.0911
-						</h2>
-						<h3 className='text-xl mb-2'>Tell us a little about your project.</h3>
-						<input type='hidden' name='form-name' value='contact' />
-						<div className={'flex '}>
-							<div
-								className={
-									'flex flex-col w-full justify-center items-center'
-								}>
-								<div
-									className={
-										'text-black '
-									}>
-									<p className='hidden'>
-										<label>
-											Don’t fill this out if you're human:{' '}
-											<input name='bot-field' />
-										</label>
-									</p>
-									<div
-										className={'flex justify-between'}
-										{...register('firstName', {
-											required: true,
-											maxLength: 20,
-										})}>
-										<label
-											htmlFor='firstName'
-											className={'w-full'}>
-											<input
-												type='text'
-												name='firstName'
-												id='firstName'
-												placeholder='First Name'
-												className={
-													'border-solid border-2  border-gray-600 my-2 pl-2 w-full flex-auto'
-												}
-											/>
-											{errors.firstName?.type ===
-												'required' &&
-												'First name is required'}
-										</label>
-										<label
-											htmlFor='lastName'
-											className='ml-2 w-full'
-											{...register('lastName')}>
-											<input
-												type='text'
-												name='lastName'
-												id='lastName'
-												placeholder='Last Name'
-												className={
-													'border-solid border-2  border-gray-600  my-2 pl-2 flex-auto w-full'
-												}
-											/>
-										</label>
-									</div>
-									<label htmlFor='email'>
+				<main lang='en'>
+					<motion.div
+						initial={{ opacity: 0.2 }}
+						animate={{ opacity: 1 }}
+						className='relative h-screen z-0 bg-[#322121]'>
+						<Image
+							src='v1620327405/Newberger%20and%20Associates/brotherton-06_ivbdxi.jpg'
+							layout='fill'
+							objectFit='cover'
+							className='opacity-50 z-0'
+						/>
+						<div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+							<div className='md:p-16 p-8 flex flex-col bg-white opacity-100 z-20 drop-shadow-xl w-screen xl:w-[30vw] lg:w-[50vw] md:w-[70vw]'>
+								<h2 className='md:text-2xl text-xl font-sans font-semibold text-center mb-2'>
+									Interested in working together?
+								</h2>
+								<h2 className='md:text-2xl text-xl font-sans font-semibold text-center mb-8 md:mb-12'>
+									{' '}
+									Let's talk. 707.961.0911
+								</h2>
+								<h3 className='text-xl mb-2'>
+									Tell us a little about your project.
+								</h3>
+								<input
+									type='hidden'
+									name='form-name'
+									value='contact'
+								/>
+								<div className={'flex '}>
+									<NetlifyFormComponent
+										onSubmit={handleSubmit(onSubmit)}
+										className={
+											'flex flex-col w-full justify-center items-center'
+										}>
 										<input
-											type='email'
-											name='email'
-											id='email'
-											placeholder='Email Address'
-											{...register('email', {
-												required: true,
-											})}
-											className={
-												'border-solid border-2  border-gray-600 justify-start w-full my-2 pl-2'
-											}
+											type='hidden'
+											name='form-name'
+											value='contact'
 										/>
-										{errors.email?.type === 'required' &&
-											'email is required'}
-									</label>
-									<label htmlFor='question'>
-										<textarea
-											{...register('question', {
-												required: true,
-											})}
-											name='question'
-											id='question'
-											rows='3'
-											placeholder="Property address and project description"
-											className={
-												'border-solid border-2  border-gray-600 w-full my-2 pl-2'
-											}
-										/>
-										{errors.question?.type === 'required' &&
-											'Please leave a brief message.'}
-									</label>
-									<button
-										type='submit'
-										className='bg-yellow-500 text-gray-800 font-semibold px-8 py-2 mx-2 m-4'>
-										Send
-									</button>
+										<div className={'text-black '}>
+											<Honeypot />
+											{netlify.success && (
+												<p>Thanks for contacting us!</p>
+											)}
+											{netlify.error && (
+												<p>
+													Sorry, we could not reach
+													our servers. Please try
+													again later.
+												</p>
+											)}
+											<div
+												className={
+													'flex justify-between'
+												}
+												>
+												<label
+													htmlFor='firstName'
+													className={'w-full'}>
+													<input
+														type='text'
+														name='firstName'
+														id='firstName'
+														placeholder='First Name'
+														className={
+															'border-solid border-2  border-gray-600 my-2 pl-2 w-full flex-auto'
+														}
+													/>
+												</label>
+												<label
+													htmlFor='lastName'
+													className='ml-2 w-full'
+													>
+													<input
+														type='text'
+														name='lastName'
+														id='lastName'
+														placeholder='Last Name'
+														className={
+															'border-solid border-2  border-gray-600  my-2 pl-2 flex-auto w-full'
+														}
+													/>
+												</label>
+											</div>
+											<label htmlFor='email'>
+												
+											
+											<input
+												type='email'
+												name='email'
+												id='email'
+												placeHolder='Email Address'
+												className={
+															'border-solid border-2  border-gray-600  my-2 pl-2 flex-auto w-full'
+														}
+											/>
+											</label>
+											<label htmlFor='question'>
+												<textarea
+													
+													name='question'
+													id='question'
+													rows='3'
+													placeholder='Property address and project description'
+													className={
+														'border-solid border-2  border-gray-600 w-full my-2 pl-2'
+													}
+												/>
+												
+											</label>
+											<button
+												type='submit'
+												className='bg-yellow-500 text-gray-800 font-semibold px-8 py-2 mx-2 m-4'>
+												Send
+											</button>
+											
+										</div>
+									</NetlifyFormComponent>
 								</div>
 							</div>
 						</div>
-					</form></div></motion.div>
+					</motion.div>
 				</main>
 			</div>
-		</>)
-	
+		</NetlifyFormProvider>
+	)
 }

066c58dbdd0bd30f15f88ed4d02b97a0649fc414
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-11-11 16:22:46 -0800

    finally working!

--- pages/contact.js
+++ pages/contact.js
@@ -55,14 +55,14 @@ export default function Contact() {
 									<form
 										action='#'
 										method='post'
-										name='contactForm'
+										name='contact'
 										data-netlify='true'
 										data-netlify-honeypot='bot-field'
 										className='mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8'>
 										<input
 											type='hidden'
 											name='form-name'
-											value='contactForm'
+											value='contact'
 										/>
 										<div>
 											

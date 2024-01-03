09bc07769adcf06729ac206ed98e1a104f1098c4
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2024-01-03 10:32:25 -0800

    Refactor menu and navigation components

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './MenuItem.module.css'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -26,7 +25,9 @@ export const MenuItem = ({ href, children }) => {
 	const router = useRouter()
 
 	return (
-		<motion.div variants={variants} role='menuitem'>
+		<motion.div variants={variants} role='menuitem'
+		initial='closed'
+		>
 			<motion.div className='z-10 overflow-hidden flex '>
 				{' '}
 				<h2
--- components/Nav.js
+++ components/Nav.js
@@ -5,26 +5,6 @@ import {Menu} from '@components/Menu'
 import { NavInfo } from '@components/NavInfo'
 import { NavPhoto } from '@components/NavPhoto'
 
-const menu = {
-	closed: {
-		clipPath: 'circle(30px at 30px 30px)',
-		transition: {
-			delay: 0.5,
-			type: 'spring',
-			stiffness: 400,
-			damping: 40,
-		},
-	},
-	open: {
-		clipPath: `circle(50px at 26px 26px)`,
-		transition: {
-			type: 'spring',
-			stiffness: 200,
-			restDelta: 2,
-		},
-	},
-}
-
 export default function Nav() {
 	const [isOpen, toggleOpen] = useCycle(false, true)
 	const containerRef = useRef(null)
@@ -32,9 +12,8 @@ export default function Nav() {
 		<>
 			<motion.nav
 				className='w-screen z-50 fixed'
-				// initial={false}
+				initial={'closed'}
 				animate={isOpen ? 'open' : 'closed'}
-				// custom={height}
 				ref={containerRef}>
 				<div className='absolute right-0'>
 					<MenuToggle toggle={() => toggleOpen()} />
